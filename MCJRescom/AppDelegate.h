50cf35bab89f3b844fcd155f954215bb54b817ba
ChuanJianMu <mcj_forwork@163.com>
2015-12-10 22:27:23 +0800

    Initial Commit

---
+++ MCJRescom/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MCJRescom
+//
+//  Created by MCJ on 15/12/10.
+//  Copyright (c) 2015年 qianfeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MCJRescom/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MCJRescom
+//
+//  Created by MCJ on 15/12/10.
+//  Copyright (c) 2015年 qianfeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
