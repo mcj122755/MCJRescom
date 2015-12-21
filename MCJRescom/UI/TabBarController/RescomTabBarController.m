65d621cdbd6875c82cbfaac27aec70be4fb61e0e
ChuanJianMu <mcj_forwork@163.com>
2015-12-21 22:44:13 +0800

    添加视图
    添加视图

--- MCJRescom/UI/TabBarController/RescomTabBarController.m
+++ MCJRescom/UI/TabBarController/RescomTabBarController.m
@@ -32,6 +32,22 @@
 
 - (void)creatViewControllers
 {
+    // 实例化模块视图
+    BSBViewController *bsbVC = [[BSBViewController alloc] init];
+    HHVViewController *hhVC = [[HHVViewController alloc] init];
+    LFDViewController *lfdVC = [[LFDViewController alloc] init];
+    XHVViewController *xhvVC = [[XHVViewController alloc] init];
+    
+    // 给模块视图包装一层UINavgation
+    UINavigationController *bsbNav = [[UINavigationController alloc] initWithRootViewController:bsbVC];
+    UINavigationController *hhNav = [[UINavigationController alloc] initWithRootViewController:hhVC];
+    UINavigationController *lfdNav = [[UINavigationController alloc] initWithRootViewController:lfdVC];
+    UINavigationController *xhvNav = [[UINavigationController alloc] initWithRootViewController:xhvVC];
+    
+    // 把视图模块添加到UITabBar上
+    self.viewControllers = @[bsbNav,hhNav,lfdNav,xhvNav];
+    
+    // 设置模块图片
     
 }
 
